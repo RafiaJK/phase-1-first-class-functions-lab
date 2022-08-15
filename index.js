0d062ab2ce546b9420668f4799a70b243d7a3592
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-08-14 23:45:52 -0400

    commit tree fa3109e9ce07ac080540cc9aaa18fc5c2554db91

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2); 
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+    return function(value) {value * multiplier;}}
+
+function fareDoubler(value) {
+    return createFareMultiplier(2);
+}
